04c543a8e4caf130d4951a50a3868758330e37b7
RKRedfern <rkredfern@gmail.com>
2021-03-26 10:24:26 -0400

    some changes to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   
 #   Character.create(name: 'Luke', movie: movies.first)
 
-tooms = Monster.create(name: "Eugene Victor Tooms", description: "Genetic mutant serial killer who was capable of squeezing his body through narrow gaps.", image_url: "http://multiversitystatic.s3.amazonaws.com/uploads/2017/10/the-x-files-tooms-featured-1000x700.jpg", likes: 0, dislikes: 0, rarity: 100)
+tooms = Monster.create(name: "Eugene Victor Tooms", description: "Genetic mutant serial killer who was capable of squeezing his body through narrow gaps.", image_url: "https://i.pinimg.com/originals/3c/9c/07/3c9c07906ad0058d47326c8e43d06d92.jpg", likes: 0, dislikes: 0, rarity: 100)
 
