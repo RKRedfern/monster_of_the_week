1cfe54e216883918b24a1803528f7d665d584a8a
RKRedfern <rkredfern@gmail.com>
2021-03-30 16:04:45 -0400

    find or create by in user action, seeds updated

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,14 +6,19 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-        user = User.create(user_params)
-        render json: { user: UserSerializer.new(user) }
+        user = User.find_or_create_by(user_params)
+        if user.save
+            render json: UserSerializer.new(user), status: :accepted
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+        end
     end
+    
 
     private
     
     def user_params
-        params.require(:user).permit(:name, :title)
+        params.require(:user).permit(:name, :title, :image_url)
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 
 
-User.create(name: "Agent Dana Scully", title: "FBI Agent, MD", image_url: "")
+User.create(name: "Agent Dana Scully", title: "FBI Agent, MD", image_url: "https://imgur.com/913S03l")
 
 Category.create(name: "Humanoid")
 
 Monster.create(name: "Jersey Devil", description: "Shadowy figure spotted in rural New Jersey - Deceased, may have produced offspring", image_url: "", rarity: 86, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
 
-Monster.create(name: "Eugene Victor Tooms", description: "Genetic mutant serial killer who was capable of squeezing his body through narrow gaps.", image_url: "", rarity: 100, fav: true, user_id: 1, category_id: 1,  category_name: "Humanoid")
+Monster.create(name: "Eugene Victor Tooms", description: "Genetic mutant serial killer who was capable of squeezing his body through narrow gaps.", image_url: "https://imgur.com/5wiEWJb", rarity: 100, fav: true, user_id: 1, category_id: 1,  category_name: "Humanoid")
