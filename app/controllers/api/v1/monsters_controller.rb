a3f5926423b12848dbcd1af8616724e3e355b8c6
RKRedfern <rkredfern@gmail.com>
2021-04-05 12:46:48 -0400

    nested route and monster controller changed

--- app/controllers/api/v1/monsters_controller.rb
+++ app/controllers/api/v1/monsters_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::MonstersController < ApplicationController
 
     def index
-        monsters = Monster.all
+        user = User.find_by_id(params[:user_id])
+
+        monsters = user.monsters
         render json: MonsterSerializer.new(monsters)
     end
 
     def update
         monster = Monster.find_by_id(params[:id])
         monster.toggle! :fav
-        
+        render json: monster
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :monsters, only: [:index, :update]
-      resources :users
+        resources :users do
+          resources :monsters
+      end
     end
   end
 end
