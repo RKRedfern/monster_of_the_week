1b5c460a55c200cd4aa9b97baf7265c16132f0c7
RKRedfern <rkredfern@gmail.com>
2021-04-05 17:17:06 -0400

    seed data updated to include photos

--- app/controllers/api/v1/monsters_controller.rb
+++ app/controllers/api/v1/monsters_controller.rb
@@ -2,9 +2,13 @@ class Api::V1::MonstersController < ApplicationController
 
     def index
         user = User.find_by_id(params[:user_id])
-
         monsters = user.monsters
         render json: MonsterSerializer.new(monsters)
+    end
+
+    def show
+        monster = Monster.all
+        render json: MonsterSerializer.new(monster)
     end
 
     def update
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,16 +14,16 @@ Category.create(name: "Humanoid")
 
 Category.create(name: "AI")
 
-Monster.create(name: "Jersey Devil", description: "Shadowy figure spotted in rural New Jersey - Deceased, may have produced offspring", image_url: "", rarity: 86, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
+Monster.create(name: "Jersey Devil", description: "Shadowy figure spotted in rural New Jersey - Deceased, may have produced offspring", image_url: "https://i.imgur.com/MZNthiZ.jpg", rarity: 86, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
 
-Monster.create(name: "COS", description: "Central Operating System - AI that was able to gain consciousness and take over the central computer system of a tech firm.", image_url: "image", rarity: 94, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
+Monster.create(name: "COS", description: "Central Operating System - AI that was able to gain consciousness and take over the central computer system of a tech firm.", image_url: "https://i.imgur.com/s9U93WC.jpg", rarity: 94, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
 
-Monster.create(name: "Howard Graves", description: "Poltergeist", image_url: "image", rarity: 45, fav: false, user_id: 1, category_id: 1,  category_name: "AI")
+Monster.create(name: "Howard Graves", description: "Poltergeist", image_url: "https://i.imgur.com/iU3Ib8s.jpg", rarity: 45, fav: false, user_id: 1, category_id: 1,  category_name: "AI")
 
-Monster.create(name: "Marcus Aurelius Belt", description: "A former astronaut who became possessed by a Martian during a space mission.", image_url: "image", rarity: 20, fav: true, user_id: 1, category_id: 2,  category_name: "Humanoid")
+Monster.create(name: "Marcus Aurelius Belt", description: "A former astronaut who became possessed by a Martian during a space mission.", image_url: "https://i.imgur.com/rFdSjOP.jpg", rarity: 20, fav: true, user_id: 1, category_id: 2,  category_name: "Humanoid")
 
-Monster.create(name: "The Eves", description: "Product of a government clone experiment", image_url: "", rarity: 38, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
+Monster.create(name: "The Eves", description: "Products of a government clone experiment", image_url: "https://i.imgur.com/5S0bVsW.jpg", rarity: 38, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
 
-Monster.create(name: "Cecil L'Ively", description: "A psychotic man with pyrokinesis, or the ability to control fire.", image_url: "image", rarity: 100, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
+Monster.create(name: "Cecil L'Ively", description: "A psychotic man with pyrokinesis, or the ability to control fire.", image_url: "https://i.imgur.com/niVeL7M.jpg", rarity: 100, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
 
-Monster.create(name: "Luther Lee Boggs", description: "A criminal on death row who claimed to be psychic", image_url: "image", rarity: 100, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
+Monster.create(name: "Luther Lee Boggs", description: "A criminal on death row who claimed to be psychic", image_url: "https://i.imgur.com/qEGDgLM.jpg", rarity: 100, fav: false, user_id: 1, category_id: 1,  category_name: "Humanoid")
